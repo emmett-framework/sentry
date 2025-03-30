3b9b116491b913c00c4be4e71750670c23e728aa
Giovanni Barillari <g@baro.dev>
2025-03-30 16:37:56 +0200

    Fix version check in `_imports`

--- emmett_sentry/_imports.py
+++ emmett_sentry/_imports.py
@@ -1,7 +1,7 @@
 try:
     from emmett.__version__ import __version__
 
-    _major, _minor, _ = __version__.split(".")
+    _major, _minor, _ = map(int, __version__.split("."))
     if _major < 2 or (_major == 2 and _minor < 6):
         from .__version__ import __version__ as extver
 
